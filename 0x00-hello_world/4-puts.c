c59c954d6b4a0605347cd72535fdc17076300c7e
sammyALX <samuelhailu49@gmail.com>
2021-08-26 13:20:49 -0700

    Add myfirst c program file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ *  main -entry point of the program
+ *  in this case simpley prints a string
+ *  Return: zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
