7487bcafcf20b20a2abdb527975cdb278c132bf7
sammyALX <samuelhailu49@gmail.com>
2021-08-26 13:28:44 -0700

    add 5-print.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ *  main -entry point of the program
+ *  in this case simpley prints a string
+ *  Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
