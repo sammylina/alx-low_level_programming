d06ff441db1ac3a3c002b8eb6c9830f38af4f726
sammyALX <samuelhailu49@gmail.com>
2021-08-27 13:09:08 -0700

    Fix: change five to 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 	if (last > 5)
-		printf("Last digit of %d is %d and is greater than five\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	else if (last < 6 && !0)
