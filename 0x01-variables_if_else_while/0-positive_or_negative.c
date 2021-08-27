c47ae138bc2dfbf99b29c45ffbc3da9c205415fe
sammyALX <samuelhailu49@gmail.com>
2021-08-27 11:43:42 -0700

    add if else to check if numbers is +, - or 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* main - prints the value of 
+ * n is negative, positive or zero
+ * Return: 0 if exits coreectyle
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
