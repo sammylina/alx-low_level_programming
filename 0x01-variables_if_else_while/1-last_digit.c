020641fb18ff9d16d7e70cdbe34536252a2277f9
sammyALX <samuelhailu49@gmail.com>
2021-08-27 13:07:08 -0700

    Fix: line longer that 80 chars.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,16 @@
 
 int main(void)
 {
-	int n, lastDigit;
+	int n, last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
-	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than five\n", n, lastDigit);
-	else if(lastDigit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	else if (lastDigit < 6 && !0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than five\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6 && !0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
