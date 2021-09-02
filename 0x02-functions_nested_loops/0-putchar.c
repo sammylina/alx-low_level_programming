76166c286ef2de8c907183d3546ee37e19159a80
sammyALX <samuelhailu49@gmail.com>
2021-09-02 09:41:15 -0700

    Add header file to 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry of the program
+ *
+ * Desription: include _putchar from another file
+ * and print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return 0;
+}
