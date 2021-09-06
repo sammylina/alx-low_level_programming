609dd94e27124f831013e2f2a91b652f1313ee4f
sammyALX <samuelhailu49@gmail.com>
2021-09-06 13:55:15 -0700

    use pointers to chage param value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the param value to 98
+ * @n: takes a pointer
+ *
+ * Description: learning pointers
+ * Return: Nothing
+ */
+
+void return_to_98(int *n)
+{
+	*n = 89;
+}
