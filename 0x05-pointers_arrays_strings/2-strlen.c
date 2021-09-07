b1b4d8efefcdc96dd3603107100b4d08ae9b3879
sammyALX <samuelhailu49@gmail.com>
2021-09-07 05:38:07 -0700

    Add function to count length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strlen - find the length os a string
+ * @s: string type
+ *
+ * Description: the fuction takes one string
+ * and count howmany chars it has
+ * Return: The numbers of chars
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,1 @@
-void swap_int(int *a, int *b);
+int _strlen(char *s);
