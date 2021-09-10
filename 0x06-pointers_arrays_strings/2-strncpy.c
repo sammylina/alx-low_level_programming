52df65def46f9a2f297389186db29b12a0332a9d
sammyALX <samuelhailu49@gmail.com>
2021-09-10 08:49:15 -0700

    fix: _strncpy add null chars after copied bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncpy - copy @n bytes of @src t @dest
@@ -14,9 +15,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int src_len = strlen(src);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		*(dest + i) = src[i];
+	if (n >= src_len)
+		i = src_len - 1;
 	for ( ; i < n; i++)
 		*(dest + i) = src[i];
 
