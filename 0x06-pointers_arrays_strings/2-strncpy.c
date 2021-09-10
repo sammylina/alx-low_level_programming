673aee0f650f6d2f2986e1e58ebca0aa62044b95
sammyALX <samuelhailu49@gmail.com>
2021-09-10 08:29:31 -0700

    Add custom _strncpy and main.h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy @n bytes of @src t @dest
+ * @dest: A pointer to destination string
+ * @src: A pointer to source string
+ * @n: number of bytes to be copied from @src to @dest
+ *
+ * Description: _strncpy take three args ans copy @n bytes of
+ * chars from @src to @dest
+ * Return: A pointer to @dest(copied string)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = src[i];
+	for ( ; i < n; i++)
+		*(dest + i) = src[i];
+
+	return (dest);
+}
+
