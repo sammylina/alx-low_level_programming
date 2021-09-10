cb1626c8eb2059c55b2f9264804258b1aafe33b4
sammyALX <samuelhailu49@gmail.com>
2021-09-10 07:16:37 -0700

    fix syntax and typo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,9 +17,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int src_len = strlen(src);
 
-	for (i = 0; i < src_len && src[i] != '\0', i++)
+	for (i = 0; i < src_len && src[i] != '\0'; i++)
 		*(dest + dest_len + i) = src[i];
-	dest[dest_len + i];
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
