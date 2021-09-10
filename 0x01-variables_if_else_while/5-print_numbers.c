0454b63c1bf5ce143d7207beea6e4a1761a133fb
sammyALX <samuelhailu49@gmail.com>
2021-09-10 12:15:39 -0700

    Add function that prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ *
+ * Description: prints numbers from 1-9
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+}
