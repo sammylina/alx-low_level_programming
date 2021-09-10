6dc0e200b46cbb1a2a7e9fe53e4918457aaad8c2
sammyALX <samuelhailu49@gmail.com>
2021-09-10 10:46:37 -0700

    Add _strcmp function and update header file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Description: it taketwo strings compare
+ * thiervalues and return the difference
+ * Return: 0 if they are equal, positibe if @s1 > @s2
+ * and negative is @s1 < @s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int ans;
+
+	ans = strcmp(s1, s1);
+
+	return (ans);
+
+}
