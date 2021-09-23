e34cf556e0c697231d5439665b832d36a54c744f
sammyALX <samuelhailu49@gmail.com>
2021-09-23 07:59:53 -0700

    Add double inclusion protection for main.h and typo fix prototype declaration

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 char *_strcat(char *dest, char *src);
@@ -5,7 +8,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strncat(char *dest, char *src, int n);
 int _atoi(char *s);
-char *_strchar(char *s, char c);
+char *_strchr(char *s, char c);
 int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
 int _islower(int c);
@@ -18,3 +21,5 @@ char *_strstr(char *haystack, char *needle);
 int _abs(int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+
+#endif
