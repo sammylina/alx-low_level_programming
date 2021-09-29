729a34da800ec16f3e6aca65872520701ac62d44
sammyALX <samuelhailu49@gmail.com>
2021-09-29 04:57:22 -0700

    Add _strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - @src string to @dest
+ * @src: source of string to copy from
+ * @dest: destination to copy to
+ *
+ * Description: Copy the string in @src to string @dest by value
+ *
+ * Return: pointer to @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
