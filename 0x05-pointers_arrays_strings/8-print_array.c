b430098ba2727c5fc94145cc3166092f86089ff1
sammyALX <samuelhailu49@gmail.com>
2021-09-29 03:42:19 -0700

    Add a function that prints an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -16,5 +15,9 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
+	{
+		printf("%d", a[i]);
+		if (i != n-1) printf(", ");
+	}
+	printf("\n");
 }
