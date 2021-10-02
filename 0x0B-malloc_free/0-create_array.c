87e29a190868c047646e15d7ad26d7620d885dc1
sammyALX <samuelhailu49@gmail.com>
2021-10-02 04:07:26 -0700

    Add: create_arryay function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - create array using the given args
+ * @size: size of array to create
+ * @c: elements of the arr
+ *
+ * Discription: create ana rray with @size size
+ * and initialize it with @c char
+ * Return: if @size  equals to 0 NULL otherwise pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr = malloc(sizeof(char) * size);
+	int i = 0;
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		*(arr + i) = c;
+		i++;
+	}
+	if (*arr == '\0')
+		return (NULL);
+	else
+		return (arr);
+}
