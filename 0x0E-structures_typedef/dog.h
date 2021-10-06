b7c1ee76b02a72901b3c3868253b958ea59b9bc8
sammyALX <samuelhailu49@gmail.com>
2021-10-06 05:24:37 -0700

    declare struct dog to the header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog type structure
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a dog type struc with three members
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
