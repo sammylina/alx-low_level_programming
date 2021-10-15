eb783640f60fce3b0a22b9a96c2214aefc1b168c
sammyALX <samuelhailu49@gmail.com>
2021-10-15 15:40:57 -0700

    change logic from loop to recursion based

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,16 +8,8 @@
  */
 
 void print_binary(unsigned long int n)
-{
-	char c;
-
-	while (n > 0)
-	{
-		c = n & 1;
-		if (c == 1)
-			_putchar('1');
-		else if (c == 0)
-			_putchar('0');
-		n >>= 1;
-	}
+{	
+	if (n > 1)
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 }
