1dfd5f5dfafdd8e5fa7d3243dffc9f38d3bf876d
sammyALX <samuelhailu49@gmail.com>
2021-10-16 19:10:39 -0700

    fix print_num able to print negative numbers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,23 +19,34 @@ void print_diagsums(int *a, int size)
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				r_sum += *((a + i*size) + j);
+				r_sum += *((a + i * size) + j);
 			if ((i + j) == (size - 1))
-				l_sum += *((a + i*size) + j);
+				l_sum += *((a + i * size) + j);
 		}
 	}
 	print_num(r_sum);
 	_putchar(',');
 	_putchar(' ');
-	print_num(r_sum);
+	print_num(l_sum);
 	_putchar('\n');
 }
+/**
+ * print_num -print integers using putchar()
+ * @n: integer number to print
+ *
+ * Return: void
+ */
 
 void print_num(int n)
 {
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 	if (n == 0)
 		_putchar('0');
 	if (n / 10)
-		print_num(n/10);
+		print_num(n / 10);
 	_putchar(n % 10 + '0');
 }
