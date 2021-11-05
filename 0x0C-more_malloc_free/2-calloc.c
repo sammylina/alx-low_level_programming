5e67a48653c89397eeb10fa6397d30bcaec13f98
sammyALX <samuelhailu49@gmail.com>
2021-11-05 06:42:33 -0700

    add functionalit that can allocate memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate memory for an arrya
+ * @nmemb: number of array elements
+ * @size: size of array element
+ *
+ * Return: a pointer to the allocated memeoty
+ * or NULL if @nmemb or size is 0, or if
+ * malloc failed
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,8 +2,7 @@
 #define _MAIN_H_
 
 #include <stdlib.h>
-#include <string.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 #endif
