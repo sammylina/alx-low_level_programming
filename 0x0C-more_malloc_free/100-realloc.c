ae20c830b8e8a57148c06ee043252830016e2149
sammyALX <samuelhailu49@gmail.com>
2021-11-08 04:51:25 -0800

    change return value to NULL when new_size is 0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 	if (ptr == NULL)
 	{
