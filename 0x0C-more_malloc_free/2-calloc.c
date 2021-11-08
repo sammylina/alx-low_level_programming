9e13b27e08b9faacfc51caa137346948359eda91
sammyALX <samuelhailu49@gmail.com>
2021-11-08 05:00:51 -0800

    rewrite _calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,3 +9,23 @@
  * or NULL if @nmemb or size is 0, or if
  * malloc failed
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr_ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr_ptr = malloc(nmemb * size);
+	if (arr_ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)arr_ptr + i) = 0;
+	}
+	return (arr_ptr);
+}
