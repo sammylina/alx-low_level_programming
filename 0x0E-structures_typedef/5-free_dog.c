aa67451e5aaf48d592bea96d9e08dc7ee928f820
sammyALX <samuelhailu49@gmail.com>
2021-11-12 15:46:40 -0800

    free allocated memory for dog_t type

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - free allocated memory for type dog
+ * @d: pointer to dog_t
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((d->name));
+		free((d->owner));
+		free(d);
+	}
+}
