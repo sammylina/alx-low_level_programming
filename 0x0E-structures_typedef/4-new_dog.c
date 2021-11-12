7699ef33cd2c9cd173224b57c9f0e3fa82d27150
sammyALX <samuelhailu49@gmail.com>
2021-11-12 15:31:56 -0800

    fix memeory leak

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,11 +21,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(strlen(name) + 1);
 	if ((d->name) == NULL)
 	{
+		free(d);
 		return (NULL);
 	}
 	d->owner = malloc(strlen(owner) + 1);
 	if ((d->owner) == NULL)
 	{
+		free((d->name));
+		free(d);
 		return (NULL);
 	}
 	d->name = strcpy((d->name), name);
