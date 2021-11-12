2b059fc28ffb1bf6817ba339563f22382af5137e
sammyALX <samuelhailu49@gmail.com>
2021-11-12 14:11:56 -0800

    define new type dog_t as type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  * Description: a dog type struc with three members
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
