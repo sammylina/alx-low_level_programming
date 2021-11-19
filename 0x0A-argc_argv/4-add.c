db1ab079b40531ce7df7b78f16529157a9834da8
sammyALX <samuelhailu49@gmail.com>
2021-11-19 04:34:15 -0800

    use  my own stoi funcion

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
+
+int stoi(char *str);
 /**
  * main - add numbers
- *@argc: number of arugment to the program
- *@argv: array of arguments
+ * @argc: number of arugment to the program
+ * @argv: array of arguments
  *
  * Return: 0 if successful
  */
+
 int main(int argc, char **argv)
 {
 	int sum, i;
@@ -20,14 +24,41 @@ int main(int argc, char **argv)
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		if (stoi(argv[i]) == -1)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		sum += stoi(argv[i]);
 	}
 	printf("%d\n", sum);
 
 	return (0);
+}
+/**
+ * stoi - conver string to integer
+ * @str: number in string type
+ *
+ * Return: converted number on success
+ * and -1 on failure
+ */
+int stoi(char *str)
+{
+	int i, len, sum;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	len = strlen(str);
+	sum = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] - '0' > 9)
+		{
+			return (-1);
+		}
+		sum = sum * 10 + (str[i] - '0');
+	}
+	return (sum);
 }
