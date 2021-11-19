7e7aeddf1e65dc4fd9a163745b270b137184be19
sammyALX <samuelhailu49@gmail.com>
2021-11-19 03:09:48 -0800

    hanle when arguments are less

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,13 @@ int main(int argc, char **argv)
 {
 	int prod;
 
-	prod = atoi(argv[1]) * atoi(argv[2]);
 	if (argc < 3)
+	{
 		printf("Error\n");
-	else
-		printf("%d\n", prod);
+		return (1);
+	}
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
 
 	return (0);
 }
