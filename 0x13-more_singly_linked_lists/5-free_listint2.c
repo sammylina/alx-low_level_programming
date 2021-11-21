63f619be63e2ec86a6a9d0b394d0d17df40a1623
SammyAlx <samuelhailu49@gmail.com>
2021-11-21 22:03:50 +0300

    add free_listint definition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,3 +12,21 @@ void free_listint2(listint_t **head)
 	free_listint(*head);
 	*head = NULL;
 }
+/**
+ * free_listint - free a linked list(listint_t )
+ * @head: point to the head of the list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head)
+	{
+		current_node = head;
+		head = current_node->next;
+		free(current_node);
+	}
+}
