5c9c7c4140fb0d934ff8c48d7793dd1d2f0ebeba
SammyAlx <samuelhailu49@gmail.com>
2021-11-21 14:24:04 +0300

    free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: head of the list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+	
+	while (head)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
