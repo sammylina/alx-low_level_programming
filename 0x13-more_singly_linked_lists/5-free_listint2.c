4d4e3a3316038dca11abc8815c50e543f5da848a
SammyAlx <samuelhailu49@gmail.com>
2021-11-21 22:10:35 +0300

    add check if pointer to list is NULL or not

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,8 +9,11 @@
 
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
-	*head = NULL;
+	if (head != NULL)
+	{
+		free_listint(*head);
+		*head = NULL;
+	}
 }
 /**
  * free_listint - free a linked list(listint_t )
