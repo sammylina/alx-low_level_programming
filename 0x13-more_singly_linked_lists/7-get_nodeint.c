5df6c452305d0a84d59e3efb9fd68b05f3838245
SammyAlx <samuelhailu49@gmail.com>
2021-11-21 22:41:35 +0300

    get node at position index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get node at position index
+ * @head: point to the head of the list
+ * @index: index of the node
+ *
+ * Return: node at position index of NULL if
+ * it dosen't exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int i;
+
+	node = head;
+	for (i = 1; index >= i; i++)
+	{
+		node = node->next;
+	}
+
+	return (node);
+}
