96489142d0f87c7bfeaba289498f98e48d5d5928
SammyAlx <samuelhailu49@gmail.com>
2021-11-21 22:22:37 +0300

    delete node form the beginning of listint_t

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete head of the list
+ * @head: point to head of the list
+ *
+ * Return: data of deleted node(head) or 0
+ * it is empity
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *head_node;
+
+	data = (*head)->n;
+	head_node = *head;
+	*head = (*head)->next;
+	free(head_node);
+
+	return (data);
+}
