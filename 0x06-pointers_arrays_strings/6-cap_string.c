9d4d3c1b91c43819541b0e5852bde3ac038eb706
sammyALX <samuelhailu49@gmail.com>
2022-02-06 13:43:26 +0300

    capitalize the first letter of words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - captialize words of a string.
+ * @str: a pointer to string
+ *
+ * Description: all characters following ,,,;,.,!,?,",(,),{,}
+ * will be capitalized
+ * Return: a pointer to string capitalized
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10)
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == 44 || str[i - 1] == 59)
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == 46 || str[i - 1] == 33)
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == 63 || str[i - 1] == 34)
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == 40 || str[i - 1] == 41)
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == 123 || str[i - 1] == 125)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
