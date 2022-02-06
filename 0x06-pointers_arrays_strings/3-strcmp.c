d80d801b7904be3aa2e8e04779b10d3ebeb9135c
sammyALX <samuelhailu49@gmail.com>
2022-02-06 11:56:39 +0300

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,8 +15,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int ans;
+	int i;
 
-	ans = strcmp(s1, s1);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ans = *(s1 + i) - *(s2 + i);
+		if (ans != 0)
+		{
+			break;
+		}
+	}
 
 	return (ans);
 
