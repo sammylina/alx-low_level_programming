8573df65ec93c5154964c00b77b2c17345170794
sammyALX <samuelhailu49@gmail.com>
2022-02-09 17:06:54 +0300

    add description to hash_table_create func

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,13 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - create hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to the newly created hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table = malloc(sizeof(hash_table_t));
