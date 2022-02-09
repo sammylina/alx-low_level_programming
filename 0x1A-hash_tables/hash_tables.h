cb69dff13b639c302f37913e98472671ea385c8e
sammyALX <samuelhailu49@gmail.com>
2022-02-09 17:02:56 +0300

    create hash table

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,39 @@
+#ifndef _HASH_TABLES_
+#define _HASH_TABLES_
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The corresponding value to the key
+ * @next: a pointer to the next node of the list
+ */
+
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: size of the array
+ * @array: array of size @size
+ * Each call of the array is pointer to the head of
+ * the linked list. because we want the hash table handle collision using
+ * chaining.
+ */
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+
+#endif
