3b6ed192debbe1aeefb63c6eea1d196a3c0b4a27
sammyALX <samuelhailu49@gmail.com>
2022-07-02 16:16:16 +0300

    add collision resolution for the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,25 +1,54 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - insert value to the hash table
+ * @ht: pointer to the hash table
+ * @key: key in the hash table
+ * @value: value for the corresponding key
+ *
+ * Return: 1 if it fails and 0 on success
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
-	const char *value_copy;
+	unsigned long int index;
+	hash_node_t *current;
 	hash_node_t *new_node;
-	unsigned long int key_idx;
-	if (key == NULL || ht == NULL || value == NULL) return (1);
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (1);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	printf("hashed index: %d\n", index);
+	current = ht->array[index];
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (1);
 	}
-
-	value_copy = strdup(value);
-	new_node->next = NULL;
+	printf("current node %p\n", current);
+	while (current)
+	{
+		if (current->key == key)
+		{
+			current->value = strdup(value);
+			return (0);
+		}
+		else if (current->next == NULL)
+		{
+			new_node->key = (char *)key;
+			new_node->value = strdup(value);
+			new_node->next = NULL;
+			current->next = new_node;
+			return (0);
+		}
+		current = current->next;
+	}
 	new_node->key = (char *)key;
-	new_node->value = (char *)value_copy;
-	key_idx = key_index((const unsigned char *)key, ht->size);
-	ht->array[key_idx] = new_node;
-	
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+	ht->array[index] = new_node;
+
 	return (0);
 }
