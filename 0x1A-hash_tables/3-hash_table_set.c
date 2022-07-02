97f4599324f74025ad7ccc2ae12b13b0043f97e6
sammyALX <samuelhailu49@gmail.com>
2022-07-02 16:59:38 +0300

    add nullity check for string duplication

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,14 +22,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 	{
 		return (0);
 	}
-	new_node->key = (char *)key;
-	new_node->value = strdup(value);
-	new_node->next = NULL;
 	head = current;
 	while (current)
 	{
@@ -50,4 +47,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = new_node;
 
 	return (1);
+}
+
+/**
+ * create_node - create a new hash node
+ * @key: key of the new hash node
+ * @value: value of the new hash node
+ *
+ * Return: newly created hash node, if fails NULL
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->key = (char *)key;
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,6 +35,11 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/**
+ * helper functions
+ */
+hash_node_t *create_node(const char *key, const char *value);
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
