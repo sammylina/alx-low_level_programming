d0f434a6558bd04b7d91daa812203af8eca8a23e
sammyALX <samuelhailu49@gmail.com>
2022-07-02 19:08:56 +0300

    print a hash table function as {'key': 'value'}

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print element key and value of hash table
+ * @ht: pointer to hash table
+ *
+ * Return: void
+ */
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	unsigned int comma_flag = 0;
+
+
+	if (ht == NULL)
+		return;
+	i = ht->size;
+	printf("{");
+	while (i)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(",");
+			current = ht->array[i];
+			while (current)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+				current = current->next;
+				if (current)
+					printf(",");
+			}
+			comma_flag = 1;
+		}
+		i--;
+	}
+	printf("}\n");
+}
